ad70a5d5b38b56c5344348f548ae68543653a737
binaryCoder-101 <syed.misak@gmail.com>
2025-10-18 10:35:25 +0530

    remove node_modules

--- src/repl.ts
+++ src/repl.ts
@@ -1,7 +1,13 @@
+import { Interface } from "readline";
+
 export function cleanInput(inputString: string): string[] {
     const newArray = inputString.trim().toLowerCase().split(" ");
     for (let i=0; i<newArray.length; i++) {
         newArray[i] = newArray[i];
     }
     return newArray;
+}
+
+interface readInput {
+    
 }
