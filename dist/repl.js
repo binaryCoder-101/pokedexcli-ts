891d73a180974f7221999516099a6e70f79f805f
binaryCoder-101 <syed.misak@gmail.com>
2025-10-19 19:32:33 +0530

    REPL logic

--- dist/repl.js
+++ dist/repl.js
@@ -1,31 +1,28 @@
 import { createInterface } from "node:readline";
 import { stdin, stdout } from "node:process";
-
-export function cleanInput(inputString: string): string[] {
+export function cleanInput(inputString) {
     const newArray = inputString.trim().toLowerCase().split(" ");
-    for (let i=0; i<newArray.length; i++) {
+    for (let i = 0; i < newArray.length; i++) {
         newArray[i] = newArray[i];
     }
     return newArray;
 }
-
 const r1 = createInterface({
     input: stdin,
     output: stdout,
     prompt: `Pokedex > `,
 });
-
 export function startREPL() {
     r1.prompt();
     r1.on("line", (input) => {
         let words = cleanInput(input);
-
-        if (words.length === 0){
+        if (words.length === 0) {
             r1.prompt();
             return;
-        } else {
+        }
+        else {
             console.log(`Your command was: ${words[0]}`);
             r1.prompt();
         }
-    })
+    });
 }
